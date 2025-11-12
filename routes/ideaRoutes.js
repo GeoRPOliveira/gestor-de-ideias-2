ddc9f74ac2379dd804a27b3b321d15734262b6fd
GeoRPOliveira <geovanna.oliveira@germinare.org.br>
2025-11-12 15:55:43 -0300

    edit e delete

--- routes/ideaRoutes.js
+++ routes/ideaRoutes.js
@@ -9,6 +9,11 @@ router.get("/", isLoggedIn, ideaController.showIdeas);
 router.get("/create", isLoggedIn, ideaController.createIdea);
 router.post("/create", isLoggedIn, ideaController.saveIdea);
 
+router.post("/:id/delete", isLoggedIn, ideaController.deleteIdea);
+
+router.get("/:id/edit", isLoggedIn, ideaController.editIdeaForm);
+router.post("/:id/edit", isLoggedIn, ideaController.updateIdea);
+
 router.get("/:id", isLoggedIn, ideaController.ideaDetails);
 
 export default router;
